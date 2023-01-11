90116d9010469cb024437bcb21314d1173bb5617
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-11 16:59:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
-  
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      enrollmentNo: "12345678",
+      age: 34,
+    };
+    this.incrementAge = this.incrementAge.bind(this);
+  }
 
+  incrementAge() {
+    this.setState((prevState) => ({ age: prevState.age + 1 }));
+  }
+
+  render() {
+    const { name, enrollmentNo, age } = this.state;
+    return (
+      <div>
+        <h1>Hello, my name is {name}</h1>
+        <p>
+          I am {age} years old and my enrollment no is {enrollmentNo}
+        </p>
+        <button onClick={this.incrementAge}>Increment Age</button>
+      </div>
+    );
+  }
+}
 
 export default App;
